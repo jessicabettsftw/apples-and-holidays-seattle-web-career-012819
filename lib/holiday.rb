ca4cdc7aa13c0242eb8220fc2cb880d1a180fba5
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 03:23:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season2}:"
     values.each do |holiday, info|
       holiday1 = holiday.to_s
-      holiday2 = holiday1.sub("_")
+      holiday2 = holiday1.sub("_", " ")
       info = info.flatten.join(", ")
       puts "  #{holiday2}: #{info}"
     end
