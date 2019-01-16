b9ae0a22d87b5dd7706bc2ca6bf315ab44428929
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 03:31:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holiday|
+  holiday_hash.collect do |season, holiday|
     season.collect do |holiday, info|
       holiday
     end
